9b75f152c83ab7c9f991f5dc00260e7cd99ac7f1
Joel Grayson <joel@joelgrayson.com>
2023-05-26 15:26:19 -0400

    preprocessed data

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
-df=pd.read_csv('./src/2020/ACS/data.csv', dtype=object)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_rows', 50)
+
+for years in ['2020', '2010']:
+    df=pd.read_csv(f'./src/{years}/ACS/data.csv', dtype=object)
+    cols_to_drop=filter(lambda x: x[-1]=='A', df.columns)
+    df=df.drop(columns=cols_to_drop)
+    df.to_csv(f'./dist/{years}-ACS-data.csv', index=False)
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd preprocessing
+python3 ./main.py
